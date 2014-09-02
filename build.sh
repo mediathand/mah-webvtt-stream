d03f1d7f0107f6dcfbb71704cbce0118b814f95d
Gil Pedersen <git@gpost.dk>
2014-09-02 13:34:55 +0200

    use https url rather than git

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -Rf telxcc
-git clone git@github.com:kanongil/telxcc.git -b output-webvtt-apple || exit 1
+git clone https://github.com/kanongil/telxcc.git -b output-webvtt-apple || exit 1
 make -C telxcc
