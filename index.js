b0359de8426123ce6f7434448a85f90aedf4778c
Gil Pedersen <git@gpost.dk>
2016-08-31 13:59:03 +0200

    Update to require node@4 + through2

--- index.js
+++ index.js
@@ -1,55 +1,62 @@
-var spawn = require('child_process').spawn;
+'use strict';
 
-var through = require('through');
+const Spawn = require('child_process').spawn;
+const Through2 = require('through2');
 
 module.exports = WebVttStream;
 
 function WebVttStream(options) {
   options = options || {};
 
-  var telxcc = null;
-  var closed = false;
+  let telxcc = null;
+  let closed = false;
 
-  return through(function write(data) {
-    var self = this;
+  return Through2(function write(chunk, encoding, callback) {
 
-    if (!telxcc && data) {
-      var params = ['-n'];
+    if (!telxcc) {
+      let params = ['-n'];
 
       if (options.pid) params.push('-t', options.pid);
       if (options.teletextPage) params.push('-p', options.teletextPage);
       if (options.endAfter) params.push('-e', options.endAfter);
 
-      telxcc = spawn(__dirname+'/telxcc/telxcc', params, { stdout: ['pipe', 'pipe', 'ignore'] });
+      telxcc = Spawn(`${__dirname}/telxcc/telxcc`, params, { stdout: ['pipe', 'pipe', 'ignore'] });
 
-      telxcc.stdin.on('close', function() {
+      telxcc.stdin.on('close', () => {
+
         closed = true;
       });
 
-      telxcc.stdin.on('error', function(err) {
+      telxcc.stdin.on('error', (err) => {
+
         if (err.code !== 'EPIPE' && err.code !== 'ECONNRESET') throw err;
       });
 
-      telxcc.stdout.on('data', function(chunk) {
-        self.queue(chunk);
+      telxcc.stdout.on('data', (chunk) => {
+
+        this.push(chunk);
       });
 
-      telxcc.stdout.on('end', function() {
-        self.queue(null);
-      });
+      telxcc.on('close', (code) => {
 
-      telxcc.on('close', function(code) {
         if (code !== 0) {
-          console.log('process exited with code ' + code);
+          console.log(`process exited with code ${code}`);
         }
       });
     }
 
-    if (data && !closed && telxcc.stdin.writable)
-      telxcc.stdin.write(data);
-  },
-  function end () { //optional
-    if (telxcc) telxcc.stdin.end();
-    else this.queue(null);
+    if (chunk && !closed && telxcc.stdin.writable) {
+      telxcc.stdin.write(chunk);
+    }
+
+    callback();
+  }, (callback) => {
+
+    if (telxcc) {
+      telxcc.stdout.on('end', callback);
+      telxcc.stdin.end();
+    } else {
+      callback();
+    }
   });
 }
